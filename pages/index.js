e58e4bce6e1a226e6c9c820066021c4bed76bda6
Ashleigh Williams <90770698+AshleighWilliams@users.noreply.github.com>
2021-12-08 20:08:00 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,17 @@
 //import layout from components.
 import Layout from '../components/Layout.js'; 
 
+import Head from 'next/head';
+
 //import layout for homepage with a title and styling of this title.
 const Home = () => (
     <div>
+    
+    <Head async src="https://www.googletagmanager.com/gtag/js?id=G-5H189Y8QK5"></Head>
+    <Head>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}gtag('js',new Date());
+    gtag('config', 'G-5H189Y8QK5');
+    </Head>
+
         <Layout />
         <h1>Hi, I'm Ashleigh Williams!</h1>
         <small>// An Aspiring Web Developer  </small>
