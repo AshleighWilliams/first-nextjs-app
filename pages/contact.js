c8d73cd9601ccdbabd4e49e2ec1fcca3d70fb612
Ashleigh Williams <90770698+AshleighWilliams@users.noreply.github.com>
2021-12-08 20:27:21 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -9,7 +9,7 @@ const Contact = () => (
   
   <Head async src="https://www.googletagmanager.com/gtag/js?id=G-5H189Y8QK5"></Head>
     <Head>window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}gtag('js',new Date());
-    gtag('config', 'G-5H189Y8QK5');
+    gtag('config', 'G-5H189Y8QK5')
     </Head>
 
     <Layout />
