07a64f4b518f30a353d1a012f49319a34e98e8b5
Ashleigh Williams <90770698+AshleighWilliams@users.noreply.github.com>
2021-12-09 17:09:16 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -9,11 +9,13 @@ import Layout from "../components/Layout";
 const About = () => (
     <div>
     
-    <Head async src="https://www.googletagmanager.com/gtag/js?id=G-5H189Y8QK5"></Head>
-    <Head>window.dataLayer = window.dataLayer || []; 
+     <Head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-5H189Y8QK5"></script>
+    <script>window.dataLayer = window.dataLayer || []; 
           function gtag(){dataLayer.push(arguments)};
           gtag('js',new Date());
           gtag('config', 'G-5H189Y8QK5');
+    </script>
     </Head>
 
         <Layout />
