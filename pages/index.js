8ce7ae09cade7081dc01d854008abef377cb6339
Ashleigh Williams <90770698+AshleighWilliams@users.noreply.github.com>
2022-01-06 16:43:35 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,43 @@
 //this index.js file acts as the homepage.
 
 //import layout from components.
-import Layout from '../components/Layout.js'; 
+import Layout from '../components/Layout.js';
 
-//import layout for homepage with a title and styling of this title.
+//import layout for homepage with a title, gif and images with links with styling of all of this.
 const Home = () => (
     <div>
         <Layout />
+        <div>
+        <img className='webdev' src="/static/69723-web-developer.gif" alt="webdev" background="transparent" speed="1" loop autoplay />
+        </div>
+        <br/>
+        <div className='one'>
         <h1>Hi, I'm Ashleigh Williams!</h1>
         <small>// An Aspiring Web Developer  </small>
-        
+        <br/>
+        <br/>
+        <small> 
+        <a href="https://www.linkedin.com/in/ashleigh-williams-07b54b203/">
+            <img className='images' src="/static/linkedin.png" alt="linkedin" />
+        </a>
+        <a href="https://github.com/AshleighWilliams">
+            <img className='images' src="/static/github.png" alt="github" />
+        </a>
+        <a href="https://webdevash.blogspot.com/">
+            <img className='images' src="/static/blog.png" alt="blog" />
+        </a>
+        </small>
+        </div>
         <style jsx>{`
+        .images {
+            width: 40px;
+            height: 40px;
+        }
+        .webdev {
+            width: 450px;
+            height: 480px;
+            float: right;
+        }
         h1 {
             font-family: 'DM Serif Display', serif;
             font-size: 5rem;
@@ -21,12 +48,10 @@ const Home = () => (
         small {
             font-family: 'DM Serif Display', serif;
             font-size: 2rem;
-            text-align: center;
-            margin-left: 510px;
             padding: 4rem;
             color: white;
         }
-        div {
+        .one {
             background-color: black;
         }
         `}
